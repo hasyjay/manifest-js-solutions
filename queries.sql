187c7e34d2ccea04235eb752874bf01237a2297f
hasyjay <50156808+hasyjay@users.noreply.github.com>
2020-05-13 20:16:22 +0100

    mysql queries for "teamhassanabdullahi" table
    all questions answered aside number 7

---
+++ queries.sql
@@ -0,0 +1,38 @@
+1. SELECT COUNT(ID) FROM teamhassanabdullahi;
+
+
+2. SELECT COUNT(department) FROM teamhassanabdullahi WHERE department= 'Accounting';  
+
+
+3.SELECT COUNT(status) FROM teamhassanabdullahi WHERE status= 'Pending';
+
+
+4. SELECT * FROM teamhassanabdullahi LIMIT 5;
+
+5. SELECT * FROM teamhassanabdullahi WHERE age BETWEEN 19 AND 25;
+
+6. SELECT * FROM teamhassanabdullahi WHERE DATE_SUB("09-01-2020", INTERVAL 10 DAY); 
+
+
+7. 
+
+8. UPDATE teamhassanabdullahi SET status='Activated' WHERE status='Pending';
+
+9. UPDATE teamhassanabdullahi SET Name = 'Timothy' WHERE Name = 'Tim';
+
+		10.
+
+(i) SELECT COUNT(department) FROM teamhassanabdullahi WHERE department= 'Accounting'; 
+
+(ii) SELECT COUNT(department) FROM teamhassanabdullahi WHERE department= 'Engineering';
+
+(iii) SELECT COUNT(department) FROM teamhassanabdullahi WHERE department= 'Management'; 
+
+(iv) SELECT COUNT(department) FROM teamhassanabdullahi WHERE department= 'Security';  
+
+  
+
+
+
+
+    
